a8152d27fd8ee17c898d63567a2032189a527943
SnkyPanther <alexandr.kochenkov@wsu.edu>
2023-11-06 14:55:18 -0800

    Added a table and working links for each ID

--- silverpi/src/app/home/agreements/page.js
+++ silverpi/src/app/home/agreements/page.js
@@ -14,7 +14,7 @@ import {
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-
+import Link from 'next/link';
 
 const homeButtonStyles = {
     position: 'absolute',
@@ -27,7 +27,7 @@ const homeButtonStyles = {
 //    {name 'Dashboard', href: '/home', icon: HomeIcon} 
 //]
 
-function AgreementComponent({ agreements }) {
+/*function AgreementComponent({ agreements }) {
     return (
         <div>
             <h1>Agreements</h1>
@@ -40,6 +40,8 @@ function AgreementComponent({ agreements }) {
         </div>
     );
 }
+*/
+
 
 
 
@@ -47,7 +49,7 @@ export default function Agreements() {
     const agreements = agreementsData();
     return (
         <div className="bg-grey">
-            <a href="/home" style={homeButtonStyles}>
+            <Link href="/home" style={homeButtonStyles}>
                 <button
                     type="button"
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm 
@@ -57,15 +59,16 @@ export default function Agreements() {
                     <HomeIcon className="w-6 h-6" />
                     <span>Dashboard</span>
                 </button>
-            </a>
+            </Link>
             <div className="bg-grey px-6 py-24 sm:py-32 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
                     <h1 className="text-8xl font-bold tracking-tight text-gray-900 ">Agreements</h1>
                 </div>
             </div>
+            {/*
             {agreements.map((agreement) => (
                 <div className="bg-grey px-4 sm:px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl text-center position: relative">
+                    <div className="mx-auto max-w-4xl text-center position: relative">
                         <div className= "bg-white -mx-4 mt-10 pt-11 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                             <h2 className="position absolute top-0 mt-3 ml-3 text-2xl font-semibold leading-6 text-gray-900">
                                 {agreement.title}
@@ -77,6 +80,62 @@ export default function Agreements() {
                     </div>
                 </div>
             ))}
+            */}
+            <div className="bg-grey px-4 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-4xl text-center position: relative">
+                    <div className= "bg-white -mx-4 mt-10 pt-11 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <h2 className="position absolute top-0 mt-3 ml-3 text-2xl font-semibold leading-6 text-gray-900">
+                            All Agreements
+                        </h2>
+                        <table className="min-w-full divide-y divide-gray-300">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                        ID
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Title
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Date
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Status
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Description
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200 bg-white">
+                                {agreements.map((agreement) => (
+                                    <tr key={agreement.id}>
+                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-left font-medium text-gray-900 sm:pl-6">
+                                            <Link href={`/home/agreements/${agreement.id}`} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                                                {agreement.id}
+                                            </Link>
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-left text-gray-500">
+                                            {agreement.title}
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-left text-gray-500">
+                                            {agreement.date}
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-left text-gray-500">
+                                            {agreement.active}
+                                        </td>
+                                        <td className="max-w-[200px] whitespace-normal px-3 py-4 text-left text-gray-500">
+                                            {agreement.context}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
 
         {/*<ul>
             {agreements.map((agreement) => (
@@ -91,7 +150,46 @@ export default function Agreements() {
     );
 }
 
+
+
 function agreementsData(){
+    return [
+        {
+            id: 12,
+            title: 'Equipment agreement',
+            date: '12-13-2003',
+            active: 'true',
+            context: 
+            `
+            We will be using our equipement.
+            `
+        },
+        {
+            id: 32,
+            title: 'Concrete company',
+            date: '12-1-2022',
+            active: 'true',
+            context:
+            `
+            We have an agreement with ALPHA Ready Mix for concrete.
+            `
+        },
+        {
+            id: 43,
+            title: 'Lumber agreement',
+            date: '11-30-2000',
+            active: 'false',
+            context:
+            `
+            We have an agreement with Home Depot for being our primary suplier for a discount.
+            `
+        }
+    ]
+}
+
+
+
+/*function agreementsData(){
     return [
         {
             id: 1,
@@ -164,3 +262,4 @@ function agreementsData(){
         }
     ];
 }
+*/
