82cd99915b8fc66cffe81507e478d7492be02164
alzumuhWSU <m.al-zughir@wsu.edu>
2023-11-12 15:09:57 -0800

    removed breadcrumbs from layout.js for now until final design decision is made

--- silverpi/src/app/home/layout.js
+++ silverpi/src/app/home/layout.js
@@ -1,11 +1,9 @@
 import Navbar from "../components/navbar.js"
-import BreadcrumbGenerator from "../components/breadcrumbgenerator.js";
 
 export default function DashboardLayout({ children }) {
     return (
         <>
             <Navbar />
-            <BreadcrumbGenerator></BreadcrumbGenerator>
             <section>{children}</section>
         </>
     );
