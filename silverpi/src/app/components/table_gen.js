8288d7c04ad11885b52bdd3717eebc48e77517b8
joehernandezlopez <joe.hernandezlopez@wsu.edu>
2023-11-13 14:16:53 -0800

    added basic layout for making a table with react

--- silverpi/src/app/components/table_gen.js
+++ silverpi/src/app/components/table_gen.js
@@ -1,1 +1,22 @@
 //testing file commit 
+import React from 'react';
+import { useTable, useSortBy } from 'react-table';
+
+
+const generateTable = (columns, data, styles) => {
+ const {
+   getTableProps,
+   getTableBodyProps,
+   headerGroups,
+   rows,
+   prepareRow,
+ } = useTable({ columns, data }, useSortBy);
+
+
+ return (
+        <div></div>
+    );
+};
+
+
+export default generateTable;
