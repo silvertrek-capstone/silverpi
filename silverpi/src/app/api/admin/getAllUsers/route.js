4d8669e921f8badcfb4fab86b9d0c48c7d07c718
shawyan-t <shawyanrtg@gmail.com>
2023-11-16 21:02:37 -0800

    Added SQL fetching and filtering for get All users

--- silverpi/src/app/api/admin/getAllUsers/route.js
+++ silverpi/src/app/api/admin/getAllUsers/route.js
@@ -23,7 +23,19 @@ export async function POST(request) {
         // Filter the users array by the customer to users array
         // Change Select . doesnt need JS if wanted
 
-        return NextResponse.json({ data: data.users, error }, { status: 200 })
+        const { data: sqlTab, error: sqlErr } = await supabase.from('customer_to_user').select()
+
+        if (sqlErr) {
+        console.log(sqlErr)
+        } else {    
+        console.log(sqlTab)
+        }
+
+        const filteredUsers = users.filter(user => 
+            sqlTab.some(customer => customer.user_id === user.id)
+        );
+
+        return NextResponse.json({ data: filteredUsers, error }, { status: 200 })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ data: null, error }, { status: 500 })
