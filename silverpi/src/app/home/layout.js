01203e88b9917b3e72c8790a65f1144ffab84872
iaad1 <judah.tanninen@wsu.edu>
2024-01-15 14:11:35 -0800

    implemented security for logins

--- silverpi/src/app/home/layout.js
+++ silverpi/src/app/home/layout.js
@@ -1,6 +1,17 @@
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import Navbar from "../components/navbar.js"
+import { redirect } from 'next/navigation.js'
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+    const cookieStore = cookies()
+    const supabase = createServerComponentClient({cookies: () => cookieStore})
+    const { data, error } = await supabase.auth.getSession()
+    const {session} = data
+    // Check if not signed in. Because all pages are a child component of this one, this should handle security
+    if (session === null || error) {
+        redirect('/login')
+    }
     return (
         <>
             <Navbar />
