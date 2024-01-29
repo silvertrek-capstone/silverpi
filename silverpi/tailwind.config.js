a99387351d90cf57699110f180f5f27ff2260f4b
alzumuhWSU <m.al-zughir@wsu.edu>
2024-01-28 20:12:04 -0800

    Created 404, updated tailwind configurations

--- silverpi/tailwind.config.js
+++ silverpi/tailwind.config.js
@@ -7,10 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: '#6699FF',
+        secondary: '#002159',
+        accent1: '#FCC963',
+        accent2: '#03D1FF',
+        neutral1: '#FFFFFF',
+        neutral2: '#B5BFCF',
+        neutral3: '#B5BFCF',
+        neutral4: '#000000',
+      },
+      fontSize: {
+        '15xl': '15rem',
+        '25xl': '25rem', // custom text size for 404 page
+        '30xl': '30rem',
       },
     },
   },
