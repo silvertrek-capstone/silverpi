cb168cc4b8559545ccb88e97acd830a20a134e92
SnkyPanther <alexandr.kochenkov@gmail.com>
2024-01-31 11:31:10 -0800

    Added the id number tag, waiting for the elements

--- silverpi/src/app/home/workorders/[id]/page.js
+++ silverpi/src/app/home/workorders/[id]/page.js
@@ -6,9 +6,12 @@ export default function WorkOrderDetail({ params }) {
     // const orderDetails = fetchOrderDetails(id);
 
     return (
-        <div>
-            <h1>Work Order Details for ID: {params.id}</h1>
-            {/* Render additional details here */}
+        <div className="bg-grey">
+            <div className="bg-grey px-6 py-24 sm:py-32 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 ">Work Orders #{params.id}</h1>
+                </div>
+            </div>
         </div>
     );
 }
