74a117376a59b760c9d2dd2fde6fa81331e5e6da
joehernandezlopez <joe.hernandezlopez@wsu.edu>
2024-03-07 15:02:34 -0800

    moved new sign up page.js to sign up dir

--- silverpi/src/app/test/page.js
+++
@@ -1,8 +0,0 @@
-import SignUpForm from '@/components/signupform'
-
-export default function Signup({searchParams}) {
-    const inviteId = searchParams.invite || ''
-    return(
-        <SignUpForm invite={inviteId}/>
-    )
-}
