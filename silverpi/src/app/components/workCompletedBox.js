fed478213b0bbe475fca74ba36f7bf01c0648954
alzumuhWSU <m.al-zughir@wsu.edu>
2024-03-08 00:08:40 -0800

    believe I'm done with main functionality gotta do error checking next

--- silverpi/src/app/components/workCompletedBox.js
+++ silverpi/src/app/components/workCompletedBox.js
@@ -3,17 +3,34 @@ import React from "react"
 import Link from 'next/link'
 import Image from 'next/image'
 import WorkCompletedCard from './workCompletedCard'
+import { useState } from 'react';
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline"
 
 
-
 function CompletedCardList(woCompletedList) {
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(5);
+
+    const handleNext = () => {
+        setStartIndex((prevStartIndex) => prevStartIndex + 5);
+        setEndIndex((prevEndIndex) => prevEndIndex + 5);
+    };
+    
+    const handlePrev = () => {
+            setStartIndex((prevStartIndex) => prevStartIndex - 5);
+            setEndIndex((prevEndIndex) => prevEndIndex - 5);
+    };
+
     woCompletedList = woCompletedList || []
     // have to take out the actual list since we are being returned an object with a woCompletedList value that points to the list
-    woCompletedList = woCompletedList.woCompletedList.slice(0, 10)
+    // {
+    //     woCompletedList: [x, x, x]
+    // }
+
+    let slicedList = woCompletedList.woCompletedList.slice(startIndex, endIndex)
 
     let arr = []
-    {woCompletedList.map((woComp, index) =>(
+    {slicedList.map((woComp, index) =>(
             arr.push(
                 <WorkCompletedCard 
                     title={"Example Title"}
@@ -29,20 +46,20 @@ function CompletedCardList(woCompletedList) {
 
     return(
         <>  
-        <div className="flex flex-row items-end">
-            <h1 className="text-3xl text-neutral3">Recent Completed Work</h1>
-            <div className="ml-auto">
-                <button className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
-                    <ArrowLeftIcon className="text-neutral3 hover:text-neutral4 h-8 w-8"></ArrowLeftIcon>
-                </button>  
-                <button className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
-                    <ArrowRightIcon className="text-neutral3 hover:text-neutral4 h-8 w-8"></ArrowRightIcon>
-                </button>  
-            </div>  
-        </div>
-        <div>
-            {arr}
-        </div>
+            <div className="flex flex-row items-end">
+                <h1 className="pb-2 text-3xl text-neutral3">Recent Completed Work</h1>
+                <div className="ml-auto">
+                    <button onClick={handlePrev} disabled={startIndex === 0} className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
+                        <ArrowLeftIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ArrowLeftIcon>
+                    </button>  
+                    <button onClick={handleNext} disabled={endIndex >= woCompletedList.woCompletedList.length}className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
+                        <ArrowRightIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ArrowRightIcon>
+                    </button>  
+                </div>  
+            </div>
+            <div>
+                {arr}
+            </div>
         </>
     )
 }
