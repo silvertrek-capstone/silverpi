7331e92677aa822f7ac425af0e9121d598d3cf9d
alzumuhWSU <m.al-zughir@wsu.edu>
2024-03-10 15:21:34 -0700

    Added double chevron icons

--- silverpi/src/app/components/workCompletedBox.js
+++ silverpi/src/app/components/workCompletedBox.js
@@ -5,12 +5,26 @@ import Image from 'next/image'
 import WorkCompletedCard from './workCompletedCard'
 import { useState } from 'react';
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline"
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from "@heroicons/react/24/outline"
+import { set } from "zod"
 
 
 function CompletedCardList(woCompletedList) {
     const [startIndex, setStartIndex] = useState(0);
     const [endIndex, setEndIndex] = useState(5);
     const [page, setPage] = useState(1);
+
+    const handleFirst = () => {
+        setStartIndex((prevStartIndex) => 0);
+        setEndIndex((prevEndIndex) => 5);
+        setPage((prevPage) => 1);
+    }
+
+    const handleLast = () => {
+        setStartIndex((prevStartIndex) => woCompletedList.woCompletedList.length - 5);
+        setEndIndex((prevEndIndex) => woCompletedList.woCompletedList.length);
+        setPage((prevPage) => numPages);
+    }
 
     const handleNext = () => {
         setStartIndex((prevStartIndex) => prevStartIndex + 5);
@@ -54,19 +68,25 @@ function CompletedCardList(woCompletedList) {
                 <div className="flex flex-row items-end">
                     <h1 className="pb-2 lg:text-2xl md:text-2xl sm:text-xl text-neutral3">Recent Completed Work</h1>
                     <div className="ml-auto">
+                        <button onClick={handleFirst} disabled={startIndex === 0} className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
+                            <ChevronDoubleLeftIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ChevronDoubleLeftIcon>
+                        </button>  
                         <button onClick={handlePrev} disabled={startIndex === 0} className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
                             <ArrowLeftIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ArrowLeftIcon>
                         </button>  
                         <button onClick={handleNext} disabled={endIndex >= woCompletedList.woCompletedList.length}className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
                             <ArrowRightIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ArrowRightIcon>
-                        </button>  
+                        </button> 
+                        <button onClick={handleLast} disabled={endIndex >= woCompletedList.woCompletedList.length} className="rounded-md active:bg-opacity-40 hover:bg-neutral2 hover:bg-opacity-10">
+                            <ChevronDoubleRightIcon className="font-bold text-neutral3 hover:text-neutral4 h-6 w-8"></ChevronDoubleRightIcon>
+                        </button> 
                     </div>  
                 </div>
                 <div>
                     {arr}
                 </div>
                 <div className="flex flex-row justify-center">
-                    <h3 className="justify-center">{`page ${page} of ${numPages}`}</h3>
+                    <h3>{`page ${page} of ${numPages}`}</h3>
                 </div>
             </div>
         </>
